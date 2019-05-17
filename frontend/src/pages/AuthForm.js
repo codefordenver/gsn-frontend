6f4d96ea6471b4a9d73f659f1155465fc692ae7c
Brenden Thornton <thorntonbrenden@gmail.com>
2019-05-16 19:58:01 -0600

    created one AuthForm that will take care of rendering the form for logging in or registering

---
+++ frontend/src/pages/AuthForm.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { InputBase, Button } from '@material-ui/core';
+
+const AuthForm = (props) => {
+  const {
+    classes: input,
+    error,
+    loading,
+    // logIn,
+    setUsername,
+    username,
+    password,
+    setPassword,
+    submitted,
+    completeLogin,
+    buttonLabel,
+  } = props;
+
+  return (
+      <>
+          {/* TODO Add Error Field */}
+          {error && (
+          <p>
+    Error:
+              {error}
+          </p>
+          )}
+
+          <InputBase
+            className={input}
+            onChange={e => setUsername(e.target.value)}
+            type="text"
+            error={submitted && !username}
+            name="username"
+            placeholder="User Name"
+            value={username}
+            disabled={loading}
+            onKeyPress={e => (e.key === 'Enter' ? completeLogin() : () => {})}
+            fullWidth
+          />
+
+          <InputBase
+            className={input}
+            onChange={e => setPassword(e.target.value)}
+            type="password"
+            error={submitted && !password}
+            name="password"
+            placeholder="Password"
+            value={password}
+            disabled={loading}
+            onKeyPress={e => (e.key === 'Enter' ? completeLogin() : () => {})}
+            fullWidth
+          />
+
+          <Button
+            onClick={completeLogin}
+            disabled={loading}
+            variant="contained"
+            color="secondary"
+            fullWidth
+          >
+              {buttonLabel}
+          </Button>
+      </>
+  );
+};
+
+AuthForm.propTypes = {
+  loading: PropTypes.bool,
+  setUsername: PropTypes.func,
+  username: PropTypes.func,
+  password: PropTypes.func,
+  setPassword: PropTypes.func,
+  submitted: PropTypes.func,
+  completeLogin: PropTypes.func,
+  buttonLabel: PropTypes.func,
+  error: PropTypes.objectOf(PropTypes.string),
+  classes: PropTypes.shape({
+    input: PropTypes.string,
+  }),
+};
+
+export default AuthForm;
