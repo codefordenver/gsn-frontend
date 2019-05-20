d44743ea5df815844df4a3ff827011dd8a91783e
hannahkamundson <hannahkamundson@gmail.com>
2019-05-19 20:07:26 -0600

    Added more servicse to studentServices for new pages

--- frontend/src/services/studentServices.js
+++ frontend/src/services/studentServices.js
@@ -3,3 +3,9 @@ export const getStudents = () => fetch('/mockdata/students.json')
 
 export const getStudentDetail = () => fetch('/mockdata/studentDetail.json')
   .then(result => result.json());
+
+export const getStudentGradeDetail = () => fetch('/mockdata/studentGradeDetail.json')
+  .then(result => result.json());
+
+  export const getStudentAttendanceDetail = () => fetch('/mockdata/studentAttendanceDetail.json')
+  .then(result => result.json());
