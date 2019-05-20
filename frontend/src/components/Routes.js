385af337249857f8ddf03f9dd43a0b89921b5ca4
hannahkamundson <hannahkamundson@gmail.com>
2019-05-19 20:10:59 -0600

    Added routes for new pages that were added such as /school, /student/attendance, /student/grade, /course, /courses, /course/grade

--- frontend/src/components/Routes.js
+++ frontend/src/components/Routes.js
@@ -7,8 +7,13 @@ import HomePage from 'components/HomePage';
 import LoginContainer from 'pages/Login/LoginContainer';
 import SignupContainer from 'pages/Register';
 import Districts from 'components/Districts';
-import Students from 'pages/Students';
-import StudentDetail from 'pages/StudentDetail';
+import Students from 'pages/Student/Students';
+import StudentDetail from 'pages/Student/StudentDetail';
+import StudentGradeDetail from 'pages/Student/StudentGradeDetail';
+import CourseDetail from 'pages/Course/CourseDetail';
+import SchoolDetail from 'pages/School/SchoolDetail';
+import CourseGradeDetail from 'pages/Course/CourseGradeDetail';
+import Courses from 'pages/Course/Courses';
 
 import PrivateRoute from './PrivateRoute';
 // username: user.get('username'),
@@ -26,7 +31,13 @@ export default function () {
                       <PrivateRoute exact path="/" component={HomePage} />
                       <PrivateRoute path="/districts" component={Districts} />
                       <PrivateRoute exact path="/students" component={Students} />
-                      <PrivateRoute path="/students/:studentId" component={StudentDetail} />
+                      <PrivateRoute exact path="/students/:studentId" component={StudentDetail} />
+                      <PrivateRoute path="/students/grade/:studentId" component={StudentGradeDetail} />
+                      <PrivateRoute exact path="/course" component={Courses} />
+                      <PrivateRoute exact path="/course/:courseId" component={CourseDetail} />
+                      <PrivateRoute exact path="/course/grade/:courseId" component={CourseGradeDetail} />
+                      <PrivateRoute exact path="/school/:schoolId" component={SchoolDetail} />
+
                   </Layout>
               </Switch>
           </Switch>
