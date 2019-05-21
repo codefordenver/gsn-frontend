e7ad9df58ef45a877abc8392fc8364de67e5e7b8
Brenden Thornton <thorntonbrenden@gmail.com>
2019-05-20 20:33:06 -0600

    need to build api requests for retrieving student data

--- frontend/src/services/studentServices.js
+++ frontend/src/services/studentServices.js
@@ -1,5 +1,15 @@
-export const getStudents = () => fetch('/mockdata/students.json')
-  .then(result => result.json());
+// export const getStudents = () => fetch('/mockdata/students.json')
+//   .then(result => result.json());
 
-export const getStudentDetail = () => fetch('/mockdata/studentDetail.json')
-  .then(result => result.json());
+// export const getStudentDetail = () => fetch('/mockdata/studentDetail.json')
+//   .then(result => result.json());
+
+import { request } from './request';
+
+export const getStudents = () => request({
+  url: 'gsndb/student/',
+});
+
+export const getStudentDetail = id => request({
+  url: `gsndb/student/${id}/`,
+});
