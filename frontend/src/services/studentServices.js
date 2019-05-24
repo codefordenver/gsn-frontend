56777fc5735c2a52ff15ec82a010f2af7f01abca
Brenden Thornton <thorntonbrenden@gmail.com>
2019-05-23 18:24:12 -0600

    renamed and moved this file into state directory

--- frontend/src/services/studentServices.js
+++
@@ -1,5 +0,0 @@
-export const getStudents = () => fetch('/mockdata/students.json')
-  .then(result => result.json());
-
-export const getStudentDetail = () => fetch('/mockdata/studentDetail.json')
-  .then(result => result.json());
