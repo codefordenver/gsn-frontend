ef956f1f4e7e7c43ade6645c9c478641f2d54ff4
Brenden Thornton <thorntonbrenden@gmail.com>
2019-05-23 20:26:29 -0600

    made some tweaks in the fetchStudents request

--- frontend/src/state/StudentActions.js
+++ frontend/src/state/StudentActions.js
@@ -1,5 +1,28 @@
-export const getStudents = () => fetch('/mockdata/students.json')
-  .then(result => result.json());
+import fetch from 'isomorphic-fetch';
+import * as types from './StudentConstants';
+
+const getStudents = students => ({
+  type: types.REQUEST_STUDENTS,
+  students,
+});
+
+const getStudent = student => ({
+  type: types.GET_STUDENT,
+  student,
+});
+
+export const fetchStudents = dispatch => fetch('http://gsndev.com/gsndb/students/', {
+  method: 'GET',
+  headers: {
+    Accept: 'application/json',
+    Authorization: `JWT ${localStorage.token}`,
+  },
+})
+  .then(response => response.json())
+  .then((students) => {
+    dispatch(getStudents(students));
+  })
+  .catch(error => (error));
 
 export const getStudentDetail = () => fetch('/mockdata/studentDetail.json')
   .then(result => result.json());
