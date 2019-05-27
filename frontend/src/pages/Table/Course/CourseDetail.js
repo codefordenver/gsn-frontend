5b13c367efcd482127fa501a8d2f1035cd0b85f1
hannahkamundson <hannahkamundson@gmail.com>
2019-05-26 23:04:01 -0600

    Removed excess code. Added student detail & students

--- frontend/src/pages/Table/Course/CourseDetail.js
+++ frontend/src/pages/Table/Course/CourseDetail.js
@@ -1,19 +1,25 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import {
-  Link as StyledLink, Typography, withStyles,
+ Typography, withStyles,
 } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import { getCourseDetail } from 'services/courseServices';
 import { DetailLink, DetailItem} from 'components/sharedStyles/Table/DetailStyles';
 import { loadingJSX } from 'components/sharedStyles/LoadingStyles';
 import { TablePageStyles } from 'components/sharedStyles/Table/TablePageStyles';
+import { CreateGradeTable, CreateStudentTable, CreateAttendanceTable } from 'components/sharedStyles/Table/CreateTablesStyle';
+
 
 
 
 function CourseDetail(props) {
   const [courseDetail, setCourseDetail] = useState({});
   const [loading, setLoading] = useState(true);
+  const {
+    classes: {
+       striped, tHead, tRow,
+    },
+  } = props;
   const { classes: { header }, match: { params } } = props;
   const courseIdParam = params;
 
@@ -31,12 +37,14 @@ function CourseDetail(props) {
   }
 
   const {
-    courseId,
     schoolName,
     schoolId,
     courseName,
     courseCode,
     courseSubject,
+    gradeSet,
+    attendanceSet,
+    studentSet
   } = courseDetail;
 
   return (
@@ -45,8 +53,19 @@ function CourseDetail(props) {
           <DetailItem k="Course Code" val={courseCode} />
           <DetailItem k="Subject" val={courseSubject} />
           <DetailLink k="School" val={schoolName} link={`/school/${schoolId}`} />
-          <DetailLink k="Students" val="Click Here" link={`/course/student/${courseId}`} />
-          <DetailLink k="Grades" val="Click Here" link={`/course/grade/${courseId}`} />
+
+          < CreateGradeTable header = {header} title = "Grades"
+          tHead = {tHead} data = {gradeSet} tRow = {tRow} 
+          striped = {striped} />
+
+          < CreateAttendanceTable header = {header} title = "Attendance"
+          tHead = {tHead} data = {attendanceSet} tRow = {tRow} 
+          striped = {striped} />
+
+          < CreateStudentTable header = {header} title = "Student"
+          tHead = {tHead} data = {studentSet} tRow = {tRow} 
+          striped = {striped} />
+
       </div>
   );
 }
