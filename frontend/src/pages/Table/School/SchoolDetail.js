69418d863d62c619d79c8b88d1508eba09890f30
hannahkamundson <hannahkamundson@gmail.com>
2019-05-26 23:02:56 -0600

    Removed a lot of the excess code. Added a schools and school detail page. These are referencing the create table components

--- frontend/src/pages/Table/School/SchoolDetail.js
+++ frontend/src/pages/Table/School/SchoolDetail.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import {
-  Link as StyledLink, Typography, withStyles,
+   Typography, withStyles,
 } from '@material-ui/core';
 import PropTypes from 'prop-types';
 
@@ -9,6 +8,9 @@ import { getSchoolDetail } from 'services/schoolServices';
 import { DetailLink } from 'components/sharedStyles/Table/DetailStyles';
 import { loadingJSX } from 'components/sharedStyles/LoadingStyles';
 import { TablePageStyles } from 'components/sharedStyles/Table/TablePageStyles';
+import { CreateGradeTable, CreateAttendanceTable, CreateStudentTable, 
+  CreateCourseTable, CreateBehaviorTable } from 'components/sharedStyles/Table/CreateTablesStyle';
+
 
 
 
@@ -16,6 +18,11 @@ function SchoolDetail(props) {
   const [schoolDetail, setSchoolDetail] = useState({});
   const [loading, setLoading] = useState(true);
   const { classes: { header }, match: { params } } = props;
+  const {
+    classes: {
+       striped, tHead, tRow,
+    },
+  } = props;
   const schoolIdParam = params;
 
   useEffect(() => {
@@ -33,19 +40,40 @@ function SchoolDetail(props) {
 
   const {
       schoolName,
-      schoolId,
       districtId,
       districtName,
+      studentSet,
+      courseSet,
+      gradeSet,
+      attendanceSet,
+      behaviorSet
   } = schoolDetail;
 
   return (
       <div>
           <Typography className={header} component="h1" variant="h4">{schoolName}</Typography>
           <DetailLink k="District Name" val={districtName} link={`/district/${districtId}`} />
-          <DetailLink k="Students" val="Click Here" link={`/school/student/${schoolId}`} />
-          <DetailLink k="Courses" val="Click Here" link={`/school/course/${schoolId}`} />
-          <DetailLink k="Grades" val="Click Here" link={`/school/grade/${schoolId}`} />
-          <DetailLink k="Attendance" val="Click Here" link={`/school/attendance/${schoolId}`} />
+
+          < CreateGradeTable header = {header} title = "Grades"
+          tHead = {tHead} data = {gradeSet} tRow = {tRow} 
+          striped = {striped} />
+
+          < CreateAttendanceTable header = {header} title = "Attendance"
+          tHead = {tHead} data = {attendanceSet} tRow = {tRow} 
+          striped = {striped} />
+
+          < CreateBehaviorTable header = {header} title = "Behavior"
+          tHead = {tHead} data = {behaviorSet} tRow = {tRow} 
+          striped = {striped} />
+
+          < CreateCourseTable header = {header} title = "Course"
+          tHead = {tHead} data = {courseSet} tRow = {tRow} 
+          striped = {striped} />
+
+          < CreateStudentTable header = {header} title = "Student"
+          tHead = {tHead} data = {studentSet} tRow = {tRow} 
+          striped = {striped} />
+
       </div>
   );
 }
