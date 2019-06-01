b24064f2563ee53637fdb377eb54f072a7db82c6
hannahkamundson <hannahkamundson@gmail.com>
2019-06-01 12:36:07 -0600

    Added feature that allows click of header to show or hide table

--- frontend/src/components/sharedStyles/Table/TableHeader.js
+++ frontend/src/components/sharedStyles/Table/TableHeader.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     Typography, 
   } from '@material-ui/core';
 
 
 function CreateTableHeader(props) {
+  const [hiddenTable, setHiddenTable] = useState(true);
+  function ChangeHiddenTable() {
+    setHiddenTable(!hiddenTable) ;
+  }
+
+  const table = hiddenTable ? null : props.table;
     return(
-        <Typography
-        variant="h4"
-        component="h1"
-        className={props.headerClassStyle}
-        >{props.title}
-        </Typography>
+        <div>
+          <Typography
+          variant="h4"
+          component="h1"
+          className={props.headerClassStyle}
+          onClick={ChangeHiddenTable}
+          >{props.title}
+          </Typography>
+          {table}
+        </div>
     );
 }
 
