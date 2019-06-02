8a588a3c9be28f11aba8ade0fc0522899cdfe9ee
Brenden Thornton <thorntonbrenden@gmail.com>
2019-06-02 16:14:13 -0600

    updated names for student constants

--- frontend/src/state/StudentConstants.js
+++ frontend/src/state/StudentConstants.js
@@ -1,2 +1,2 @@
-export const REQUEST_STUDENTS = 'REQUEST_STUDENTS';
-export const GET_STUDENT = 'GET_STUDENT';
+export const SET_STUDENTS = 'REQUEST_STUDENTS';
+export const SET_STUDENT = 'GET_STUDENT';
