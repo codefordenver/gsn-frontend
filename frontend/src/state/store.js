e9a9406fa9af92e541a5c6712a9cb94c55a067ca
Brenden Thornton <thorntonbrenden@gmail.com>
2019-06-02 19:27:32 -0600

    updated the redux devtools

--- frontend/src/state/store.js
+++ frontend/src/state/store.js
@@ -12,7 +12,7 @@ const store = createStore(
   }),
   compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
   ),
 );
 
