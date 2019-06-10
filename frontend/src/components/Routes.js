f1d555ea92e0ef7f352becfcc7f3db6f1100a7ba
SRSeaver <sabrinaseaver@gmail.com>
2019-06-09 20:23:51 -0600

    Added route for /myprograms

--- frontend/src/components/Routes.js
+++ frontend/src/components/Routes.js
@@ -23,6 +23,9 @@ import CourseDetail from 'pages/Table/Course/CourseDetail';
 import Schools from 'pages/Table/School/Schools';
 import SchoolDetail from 'pages/Table/School/SchoolDetail';
 
+import MyPrograms from 'pages/Table/Program/MyPrograms';
+
+
 
 
 import PrivateRoute from './PrivateRoute';
@@ -54,6 +57,9 @@ export default function () {
 
                       <PrivateRoute exact path="/school" component={Schools} />
                       <PrivateRoute exact path="/school/:schoolId" component={SchoolDetail} />
+
+                      <PrivateRoute exact path="/myprograms" component={MyPrograms} />
+                    >
 
                   </Layout>
               </Switch>
