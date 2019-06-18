229304da9c9da4822d87e6aafa9eefba55bb5b78
Michael Bragg <michael_bragg99@hotmail.com>
2019-06-17 20:31:28 -0600

    Add select element.

--- frontend/src/pages/CsvUpload.js
+++ frontend/src/pages/CsvUpload.js
@@ -12,6 +12,23 @@ function CsvUpload(props) {
 
   const csrv_token = useSelector(state => state.user.token);
 
+  const schools = ['trivial'];
+  let selectedSchool = schools[0];
+
+  const getSchoolOptions = function() {
+    return (
+      schools.map((school, schoolN) => {
+        return (
+          <option key={schoolN} value={school}>{school}</option>
+        )
+      })
+    )
+  }
+
+  const setSelectedSchool = function(e) {
+    selectedSchool = schools[indexOf(e.target.value)];
+  }
+
   if (!csrv_token) {
     return (
       <div>Token required</div>
@@ -27,6 +44,12 @@ function CsvUpload(props) {
       >
         Upload CSV
       </Typography>
+      <label className="school-select-label" htmlFor="school-select">School</label>
+        <select id="school-select"
+          value={selectedSchool}
+          onChange={setSelectedSchool}>
+          {this.getSchoolOptions()}
+        </select>
       <form method="post" 
         enctype="multipart/form-data"
         action={CSVEndpoint}>
