1f7d3048b837471cc6733da181ef00f688716b98
hannahkamundson <hannahkamundson@gmail.com>
2019-06-23 17:15:21 -0600

    Modified routes

--- frontend/src/components/Routes.js
+++ frontend/src/components/Routes.js
@@ -2,16 +2,8 @@ import React from 'react';
 import { Switch, Route, Router } from 'react-router-dom';
 import history from 'utils/history';
 import Layout from 'components/layouts/Default';
-// import Nav from 'components/Nav';
-import HomePage from 'components/HomePage';
 import LoginContainer from 'pages/Login/LoginContainer';
 import SignupContainer from 'pages/Register';
-
-import DistrictDetail from 'pages/Table/District/DistrictDetail';
-import ProgramDetail from 'pages/Table/Program/ProgramDetail';
-import StudentDetail from 'pages/Table/Student/StudentDetail';
-import CourseDetail from 'pages/Table/Course/CourseDetail';
-import SchoolDetail from 'pages/Table/School/SchoolDetail';
 
 import { MyStudents, AllStudents, 
   MySchools, AllSchools,
@@ -47,28 +39,29 @@ export default function () {
                       <PrivateRoute exact path="/all/district/:districtId" component={AllDistrictDetail} />
                       <PrivateRoute exact path="/my/district/:districtId" component={MyDistrictDetail} />
 
-                      <PrivateRoute exact path="/my/program" component={MyPrograms} />
-                      <PrivateRoute exact path="/all/program" component={AllPrograms} />
-                      <PrivateRoute exact path="/my/program/:programId" component={MyProgramDetail} />
-                      <PrivateRoute exact path="/all/program/:programId" component={AllProgramDetail} />
-
+                      <PrivateRoute exact path="/my/school" component={MySchools} />
+                      <PrivateRoute exact path="/all/school" component={AllSchools} />
+                      <PrivateRoute exact path="/all/school/:schoolId" component={AllSchoolDetail} />
+                      <PrivateRoute exact path="/my/school/:schoolId" component={MySchoolDetail} />
+                      
                       <PrivateRoute exact path="/my/student" component={MyStudents} />
                       <PrivateRoute exact path="/all/student" component={AllStudents} />
                       <PrivateRoute exact path="/my/student/:studentId" component={MyStudentDetail} />
                       <PrivateRoute exact path="/all/student/:studentId" component={AllStudentDetail} />
 
+                      <PrivateRoute exact path="/my/program" component={MyPrograms} />
+                      <PrivateRoute exact path="/all/program" component={AllPrograms} />
+                      <PrivateRoute exact path="/my/program/:programId" component={MyProgramDetail} />
+                      <PrivateRoute exact path="/all/program/:programId" component={AllProgramDetail} />
+
                       <PrivateRoute exact path="/my/course" component={MyCourses} />
                       <PrivateRoute exact path="/all/course" component={AllCourses} />
                       <PrivateRoute exact path="/my/course/:courseId" component={MyCourseDetail} />      
-                      <PrivateRoute exact path="/all/course/:courseId" component={AllCourseDetail} />  
+                      <PrivateRoute exact path="/all/course/:courseId" component={AllCourseDetail} /> 
 
-                      <PrivateRoute exact path="/my/school" component={MySchools} />
-                      <PrivateRoute exact path="/all/school" component={AllSchools} />
-                      <PrivateRoute exact path="/all/school/:schoolId" component={AllSchoolDetail} />
-                      <PrivateRoute exact path="/my/school/:schoolId" component={MySchoolDetail} />
 
                       <PrivateRoute exact path="/all" component={ViewAllDataHomepage} />
-                    >
+                    
 
                   </Layout>
               </Switch>
@@ -77,4 +70,3 @@ export default function () {
 
   );
 }
-
