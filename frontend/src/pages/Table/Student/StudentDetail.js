c36310246c9b7033fff6a603ac519774bdbe4871
Brenden Thornton <thorntonbrenden@gmail.com>
2019-06-23 12:41:24 -0600

    updated Redux hooks to use new api endpoints

--- frontend/src/pages/Table/Student/StudentDetail.js
+++ frontend/src/pages/Table/Student/StudentDetail.js
@@ -1,39 +1,44 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import {
-  Typography, withStyles,
-} from '@material-ui/core';
+import { Typography, withStyles } from '@material-ui/core';
 import PropTypes from 'prop-types';
 
-import { DetailLink, DetailItem, DetailTable } from 'components/sharedStyles/Table/DetailStyles';
+import {
+  DetailLink,
+  DetailItem,
+  DetailTable
+} from 'components/sharedStyles/Table/DetailStyles';
 import { loadingJSX } from 'components/sharedStyles/LoadingStyles';
 import { TablePageStyles } from 'components/sharedStyles/Table/TablePageStyles';
-import { CreateGradeTable, CreateAttendanceTable, CreateBehaviorTable } from 'components/sharedStyles/Table/CreateTablesStyle';
+import {
+  CreateGradeTable,
+  CreateAttendanceTable,
+  CreateBehaviorTable
+} from 'components/sharedStyles/Table/CreateTablesStyle';
 import CreateTableHeader from 'components/sharedStyles/Table/TableHeader';
 import { fetchStudent } from '../../../state/StudentActions';
 
-
 function StudentDetail(props) {
-  const { classes: { header }, match: { params } } = props;
   const {
-    classes: {
-      striped, tHead, tRow, tableTitle,
-    },
+    classes: { header },
+    match: { params }
   } = props;
+  const {
+    classes: { striped, tHead, tRow, tableTitle }
+  } = props;
+  const { studentId } = params;
 
   const [loading, setLoading] = useState(true);
   const studentDetail = useSelector(state => state.students.student);
   const dispatch = useDispatch();
-  // console.log(studentDetail);
 
   useEffect(() => {
-    dispatch(fetchStudent(params.studentId));
+    dispatch(fetchStudent({ accessLevel: 'all', studentId }));
     setLoading(false);
-  }, []);
+  }, [dispatch, studentId]);
 
   if (loading) {
-    return (
-      loadingJSX('Student Detail'));
+    return loadingJSX('Student Detail');
   }
 
   const {
@@ -48,77 +53,77 @@ function StudentDetail(props) {
     studentTerm,
     gradeSet,
     attendanceSet,
-    behaviorSet,
+    behaviorSet
   } = studentDetail;
 
   const gradeTable = (
-      <CreateGradeTable
-        header={header}
-        tHead={tHead}
-        data={gradeSet}
-        tRow={tRow}
-        striped={striped}
-      />
+    <CreateGradeTable
+      header={header}
+      tHead={tHead}
+      data={gradeSet}
+      tRow={tRow}
+      striped={striped}
+    />
   );
 
   const attendanceTable = (
-      <CreateAttendanceTable
-        headerClassStyle={header}
-        tHead={tHead}
-        data={attendanceSet}
-        tRow={tRow}
-        striped={striped}
-      />
+    <CreateAttendanceTable
+      headerClassStyle={header}
+      tHead={tHead}
+      data={attendanceSet}
+      tRow={tRow}
+      striped={striped}
+    />
   );
 
   const behaviorTable = (
-      <CreateBehaviorTable
-        header={header}
-        tHead={tHead}
-        data={behaviorSet}
-        tRow={tRow}
-        striped={striped}
-      />
+    <CreateBehaviorTable
+      header={header}
+      tHead={tHead}
+      data={behaviorSet}
+      tRow={tRow}
+      striped={striped}
+    />
   );
 
-
   return (
-      <div>
-          <Typography className={header} component="h1" variant="h4">{studentName}</Typography>
-          <DetailItem k="Gender" val={gender} />
-          <DetailItem k="Birthdate" val={birthdate} />
-          <DetailItem k="Grade" val={grade} />
-          <DetailItem k="Year" val={studentYear} />
-          <DetailItem k="Term" val={studentTerm} />
-          <DetailItem k="State Id" val={stateId} />
-          <DetailLink k="School" val={school} link={`/school/${schoolId}`} />
+    <div>
+      <Typography className={header} component="h1" variant="h4">
+        {studentName}
+      </Typography>
+      <DetailItem k="Gender" val={gender} />
+      <DetailItem k="Birthdate" val={birthdate} />
+      <DetailItem k="Grade" val={grade} />
+      <DetailItem k="Year" val={studentYear} />
+      <DetailItem k="Term" val={studentTerm} />
+      <DetailItem k="State Id" val={stateId} />
+      <DetailLink k="School" val={school} link={`/school/${schoolId}`} />
 
-          <CreateTableHeader
-            title="Grades"
-            table={gradeTable}
-            headerClassStyle={tableTitle}
-          />
+      <CreateTableHeader
+        title="Grades"
+        table={gradeTable}
+        headerClassStyle={tableTitle}
+      />
 
-          <CreateTableHeader
-            title="Attendance"
-            table={attendanceTable}
-            headerClassStyle={tableTitle}
-          />
+      <CreateTableHeader
+        title="Attendance"
+        table={attendanceTable}
+        headerClassStyle={tableTitle}
+      />
 
-          <CreateTableHeader
-            title="Behavior"
-            table={behaviorTable}
-            headerClassStyle={tableTitle}
-            haveCreateSaveButtonBool={true}
-          />
-      </div>
+      <CreateTableHeader
+        title="Behavior"
+        table={behaviorTable}
+        headerClassStyle={tableTitle}
+        haveCreateSaveButtonBool={true}
+      />
+    </div>
   );
 }
 
-
 StudentDetail.propTypes = {
   classes: PropTypes.object,
-  match: PropTypes.object,
+  match: PropTypes.object
 };
 
 export default withStyles(TablePageStyles)(StudentDetail);
