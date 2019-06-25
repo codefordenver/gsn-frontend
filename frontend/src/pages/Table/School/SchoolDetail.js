9bfc1412fe8611503e196a05b662c9bec2812329
Brenden Thornton <thorntonbrenden@gmail.com>
2019-06-25 12:58:18 -0600

    removed the connect component and updated the spacing to match the prettier syntax

--- frontend/src/pages/Table/School/SchoolDetail.js
+++ frontend/src/pages/Table/School/SchoolDetail.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector, connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Typography, withStyles } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import { DetailLink } from 'components/sharedStyles/Table/DetailStyles';
@@ -13,17 +13,16 @@ import {
   CreateBehaviorTable
 } from 'components/sharedStyles/Table/CreateTablesStyle';
 import CreateTableHeader from 'components/sharedStyles/Table/TableHeader';
-import mapStateToProps from 'components/sharedStyles/Table/StateToProps';
 import { fetchSchoolDetails } from '../../../state/SchoolActions';
 
 function FullSchoolDetail(props) {
   const {
-    classes: { header, striped, tHead, tRow, tableTitle },
+    classes: { header, striped, tHead, tRow, tableTitle }
   } = props;
   const params = props.match;
   const { schoolId } = params;
   const my_or_all = props.my_or_all;
-  const my_or_all_url = "/" + my_or_all;
+  const my_or_all_url = `/${my_or_all}`;
   const dispatch = useDispatch();
 
   const schoolDetail = useSelector(state => {
@@ -32,7 +31,7 @@ function FullSchoolDetail(props) {
 
   useEffect(() => {
     dispatch(fetchSchoolDetails({ accessLevel: my_or_all, schoolId }));
-  }, [dispatch, schoolId]);
+  }, [dispatch, my_or_all, schoolId]);
 
   if (!schoolDetail) {
     return loadingJSX('School Detail');
@@ -50,83 +49,98 @@ function FullSchoolDetail(props) {
   } = schoolDetail;
 
   const gradeTable = (
-    < CreateGradeTable 
-            header = {header}
-            tHead = {tHead} 
-            data = {gradeSet} 
-            tRow = {tRow} 
-            striped = {striped}
-            my_or_all_link = {my_or_all_url} />
+    <CreateGradeTable
+      header={header}
+      tHead={tHead}
+      data={gradeSet}
+      tRow={tRow}
+      striped={striped}
+      my_or_all_link={my_or_all_url}
+    />
   );
 
   const attendanceTable = (
-    < CreateAttendanceTable 
-            header = {header} 
-            tHead = {tHead} 
-            data = {attendanceSet} 
-            tRow = {tRow} 
-            striped = {striped} 
-            my_or_all_link = {my_or_all_url}/>
+    <CreateAttendanceTable
+      header={header}
+      tHead={tHead}
+      data={attendanceSet}
+      tRow={tRow}
+      striped={striped}
+      my_or_all_link={my_or_all_url}
+    />
   );
 
   const behaviorTable = (
-    < CreateBehaviorTable 
-            header = {header}
-            tHead = {tHead} 
-            data = {behaviorSet} 
-            tRow = {tRow} 
-            striped = {striped} 
-            my_or_all_link = {my_or_all_url}/>
+    <CreateBehaviorTable
+      header={header}
+      tHead={tHead}
+      data={behaviorSet}
+      tRow={tRow}
+      striped={striped}
+      my_or_all_link={my_or_all_url}
+    />
   );
 
   const courseTable = (
-    < CreateCourseTable 
-            header = {header}
-            tHead = {tHead} 
-            data = {courseSet} 
-            tRow = {tRow} 
-            striped = {striped} 
-            my_or_all_link = {my_or_all_url}/>
+    <CreateCourseTable
+      header={header}
+      tHead={tHead}
+      data={courseSet}
+      tRow={tRow}
+      striped={striped}
+      my_or_all_link={my_or_all_url}
+    />
   );
 
   const studentTable = (
-    < CreateStudentTable 
-            header = {header}
-            tHead = {tHead} 
-            data = {studentSet} 
-            tRow = {tRow} 
-            striped = {striped} 
-            my_or_all_link = {my_or_all_url}/>
+    <CreateStudentTable
+      header={header}
+      tHead={tHead}
+      data={studentSet}
+      tRow={tRow}
+      striped={striped}
+      my_or_all_link={my_or_all_url}
+    />
   );
 
   return (
-      <div>
-          <Typography className={header} component="h1" variant="h4">{schoolName}</Typography>
-          <DetailLink k="District Name" val={districtName} link={my_or_all_url + `/district/${districtId}`} />
+    <div>
+      <Typography className={header} component="h1" variant="h4">
+        {schoolName}
+      </Typography>
+      <DetailLink
+        k="District Name"
+        val={districtName}
+        link={my_or_all_url + `/district/${districtId}`}
+      />
 
-          <CreateTableHeader
-            headerClassStyle = {tableTitle}
-            title = "Grades" 
-            table = {gradeTable}
-            />
-          <CreateTableHeader
-            headerClassStyle = {tableTitle}
-            title = "Attendance" 
-            table = {attendanceTable}/>
-          <CreateTableHeader
-            headerClassStyle = {tableTitle}
-            title = "Behavior" 
-            table = {behaviorTable}
-            haveCreateSaveButtonBool={true}/>
-          <CreateTableHeader
-            headerClassStyle = {tableTitle}
-            title = "Course" 
-            table = {courseTable}/>
-          <CreateTableHeader
-            headerClassStyle = {tableTitle}
-            title = "Student" 
-            table = {studentTable}/>
-      </div>
+      <CreateTableHeader
+        headerClassStyle={tableTitle}
+        title="Grades"
+        table={gradeTable}
+      />
+      <CreateTableHeader
+        headerClassStyle={tableTitle}
+        title="Attendance"
+        table={attendanceTable}
+      />
+      <CreateTableHeader
+        headerClassStyle={tableTitle}
+        title="Behavior"
+        table={behaviorTable}
+        haveCreateSaveButtonBool={true}
+      />
+      <CreateTableHeader
+        headerClassStyle={tableTitle}
+        title="Course"
+        table={courseTable}
+      />
+      <CreateTableHeader
+        headerClassStyle={tableTitle}
+        title="Student"
+        table={studentTable}
+      />
+    </div>
   );
 }
 
@@ -135,5 +149,4 @@ FullSchoolDetail.propTypes = {
   match: PropTypes.object
 };
 
-const SchoolDetail = withStyles(TablePageStyles)(FullSchoolDetail);
-export default connect(mapStateToProps)(SchoolDetail);
+export default withStyles(TablePageStyles)(FullSchoolDetail);
