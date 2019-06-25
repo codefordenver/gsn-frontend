42e973b3a38550ae9b56746a36d8d9a017f37d1b
Brenden Thornton <thorntonbrenden@gmail.com>
2019-06-25 13:56:12 -0600

    turned the my/all constants into all caps to represent the variable as a constant. Also updated spacing to match prettier syntax

--- frontend/src/pages/Table/TablesAndData.js
+++ frontend/src/pages/Table/TablesAndData.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import {  useSelector } from 'react-redux';
-import { fetchStudents, fetchSchools } from 'state/StudentActions';
 
 import Students from 'pages/Table/Student/Students';
 import Schools from 'pages/Table/School/Schools';
@@ -14,159 +12,111 @@ import StudentDetail from 'pages/Table/Student/StudentDetail';
 import CourseDetail from 'pages/Table/Course/CourseDetail';
 import SchoolDetail from 'pages/Table/School/SchoolDetail';
 
-const my_url = "/my";
-const all_url = "/all";
+// const my_url = '/my';
+// const all_url = '/all';
 
-const my_const = "my";
-const all_const = "all";
+const MY_CONST = 'my';
+const ALL_CONST = 'all';
 
 function MyStudents(props) {
-    return(
-        <Students my_or_all={ my_const } />
-    );
+  return <Students my_or_all={MY_CONST} />;
 }
 
 function AllStudents(props) {
-    return(
-        <Students my_or_all={ all_const } />
-    );
+  return <Students my_or_all={ALL_CONST} />;
 }
 
 function MyStudentDetail(props) {
-    return(
-        <StudentDetail 
-            match={props.match.params}
-            my_or_all={my_const} />
-    );
+  return <StudentDetail match={props.match.params} my_or_all={MY_CONST} />;
 }
 
 function AllStudentDetail(props) {
-    return(
-        <StudentDetail 
-            match={props.match.params}
-            my_or_all={all_const} />
-    );
+  return <StudentDetail match={props.match.params} my_or_all={ALL_CONST} />;
 }
 
 function MySchools(props) {
-    return(
-        <Schools my_or_all={ my_const }  />
-    );
+  return <Schools my_or_all={MY_CONST} />;
 }
 
 function AllSchools(props) {
-    return(
-        <Schools my_or_all={ all_const } />
-    );
+  return <Schools my_or_all={ALL_CONST} />;
 }
 
 function MySchoolDetail(props) {
-    return(
-        <SchoolDetail  
-            match={props.match.params}
-            my_or_all={my_const}/>
-    );
+  return <SchoolDetail match={props.match.params} my_or_all={MY_CONST} />;
 }
 
 function AllSchoolDetail(props) {
-    return(
-        <SchoolDetail 
-            match={props.match.params}
-            my_or_all={all_const} />
-    );
+  return <SchoolDetail match={props.match.params} my_or_all={ALL_CONST} />;
 }
 
 function MyCourses(props) {
-    return(
-        <Courses my_or_all={ my_const } />
-    );
+  return <Courses my_or_all={MY_CONST} />;
 }
 
 function AllCourses(props) {
-    return(
-        <Courses my_or_all={ all_const } />
-    );
+  return <Courses my_or_all={ALL_CONST} />;
 }
 
 function MyCourseDetail(props) {
-    return(
-        <CourseDetail  
-            match={props.match.params}
-            my_or_all={my_const} />
-    );
+  return <CourseDetail match={props.match.params} my_or_all={MY_CONST} />;
 }
 
 function AllCourseDetail(props) {
-    return(
-        <CourseDetail 
-            match={props.match.params}
-            my_or_all={all_const} />
-    );
+  return <CourseDetail match={props.match.params} my_or_all={ALL_CONST} />;
 }
 
 function MyDistricts(props) {
-    return(
-        <Districts my_or_all={ my_const } />
-    );
+  return <Districts my_or_all={MY_CONST} />;
 }
 
 function AllDistricts(props) {
-    return(
-        <Districts my_or_all={ all_const } />
-    );
+  return <Districts my_or_all={ALL_CONST} />;
 }
 
 function MyDistrictDetail(props) {
-    return(
-        <DistrictDetail  
-            match={props.match.params}
-            my_or_all={my_const} />
-    );
+  return <DistrictDetail match={props.match.params} my_or_all={MY_CONST} />;
 }
 
 function AllDistrictDetail(props) {
-    return(
-        <DistrictDetail  
-            match={props.match.params}
-            my_or_all={all_const} />
-    );
+  return <DistrictDetail match={props.match.params} my_or_all={ALL_CONST} />;
 }
 
 function MyPrograms(props) {
-    return(
-        <Programs my_or_all={ my_const } />
-    );
+  return <Programs my_or_all={MY_CONST} />;
 }
 
 function AllPrograms(props) {
-    return(
-        <Programs my_or_all={ all_const } />
-    );
+  return <Programs my_or_all={ALL_CONST} />;
 }
 
 function MyProgramDetail(props) {
-    return(
-        <ProgramDetail  
-            match={props.match.params}
-            my_or_all={my_const} />
-    );
+  return <ProgramDetail match={props.match.params} my_or_all={MY_CONST} />;
 }
 
 function AllProgramDetail(props) {
-    return(
-        <ProgramDetail  
-            match={props.match.params}
-            my_or_all={all_const} />
-    );
+  return <ProgramDetail match={props.match.params} my_or_all={ALL_CONST} />;
 }
 
-export {  AllStudents, MyStudents,
-    MySchools, AllSchools,
-    MyCourses, AllCourses,
-    MyPrograms, AllPrograms,
-    MyDistricts, AllDistricts,
-    MyStudentDetail, AllStudentDetail, 
-    MySchoolDetail, AllSchoolDetail,
-    MyCourseDetail, AllCourseDetail,
-    MyProgramDetail, AllProgramDetail,
-    MyDistrictDetail, AllDistrictDetail };
+export {
+  AllStudents,
+  MyStudents,
+  MySchools,
+  AllSchools,
+  MyCourses,
+  AllCourses,
+  MyPrograms,
+  AllPrograms,
+  MyDistricts,
+  AllDistricts,
+  MyStudentDetail,
+  AllStudentDetail,
+  MySchoolDetail,
+  AllSchoolDetail,
+  MyCourseDetail,
+  AllCourseDetail,
+  MyProgramDetail,
+  AllProgramDetail,
+  MyDistrictDetail,
+  AllDistrictDetail
+};
