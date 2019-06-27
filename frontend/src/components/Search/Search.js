c65a09730a07babb85e224595dd9ad5d23684127
Kyle Middleton <kylemid321@gmail.com>
2019-06-27 13:52:05 -0600

    initial commit

---
+++ frontend/src/components/Search/Search.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class Search extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+        
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <textarea>
+
+                </textarea>
+                <button>Submit</button>
+            </div>
+        )
+    }
+}
---
+++ frontend/src/pages/SearchPage/SearchPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Search from '../../components/Search'
+
+export default class SearchPage extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+    render() {
+        return (
+            <Search />
+        )
+    }
+}
