7439131030568c75741d47944f62952d0b1d6998
Kyle Middleton <kylemid321@gmail.com>
2019-06-27 16:18:41 -0600

    changed textarea component to Field, add classnames

--- frontend/src/components/Search/Search.js
+++ frontend/src/components/Search/Search.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 
+import './Search.css'
+//const  { DOM: { input, select, textarea } } = React
+
 class Search extends Component {
     constructor(props) {
         super(props)
@@ -22,18 +25,22 @@ class Search extends Component {
     render() {
         const options = ['All', 'Student', 'Course', 'School', 'District', 'Program']
         return (
-            <form>
-                <div>
-                    <textarea>
-
-                    </textarea>
-                    <Field name="search-options" component="select">
+            <form className="search-form">
+               
+                    <Field 
+                        className="search-input"
+                        name="search-input" 
+                        component="textarea" />               
+                    <Field 
+                        className="search-options"
+                        name="search-options" 
+                        component="select">
                         {options.map((item, index) => (
                             <option value={item} key={index}>{item}</option>
                         ))}
                     </Field>
                     <button>Submit</button>
-                </div>
+                
             </form>
         )
     }
