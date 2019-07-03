75d58b3e76efab3400acec820d69d12d737746c8
Hannah Amundson <hannahkamundson@gmail.com>
2019-07-02 23:34:25 -0600

    Added a search page. It allows user to input a search phrase. It will return results if it isn't blank

--- frontend/src/pages/SearchPage/SearchPage.js
+++ frontend/src/pages/SearchPage/SearchPage.js
@@ -1,14 +1,49 @@
-import React from 'react';
-import Search from '../../components/Search'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchStudents } from "state/StudentActions";
+import { fetchDistricts } from "state/DistrictActions";
+import { fetchPrograms } from "state/ProgramActions";
+import { fetchSchools } from "state/SchoolActions";
+import { fetchCourses } from "state/CourseActions";
+import PropTypes from "prop-types";
+import { Typography, withStyles } from "@material-ui/core";
+import { loadingJSX } from "components/sharedStyles/LoadingStyles";
+import { TablePageStyles } from "components/sharedStyles/Table/TablePageStyles";
+import { CreateStudentTable } from "components/sharedStyles/Table/CreateTablesStyle";
+import SearchBar from "../../components/Search/Search";
+import SearchResults from "../../components/Search/SearchResults";
 
-export default class SearchPage extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {}
-    }
-    render() {
-        return (
-            <Search />
-        )
-    }
+function SearchPage(props) {
+  const {
+    classes: { header }
+  } = props;
+
+  const [field, setField] = useState({
+    searchInput: '',
+    showResults: false
+  });
+
+  const updateState = event => {
+    console.log("state was updated");
+    const { name, value } = event.target;
+    setField({ ...field, [name]: value });
+  };
+
+  const ShowResults = field.showResults ? "hello" : "goodbye";
+
+  return (
+    <div>
+      <Typography variant="h4" component="h1" className={header}>
+        Search
+      </Typography>
+      <SearchBar
+        action={updateState}
+      />
+      <br />
+      <ShowResults />
+      <SearchResults searchTerm={field.searchInput} />
+    </div>
+  );
 }
+
+export default withStyles(TablePageStyles)(SearchPage);
