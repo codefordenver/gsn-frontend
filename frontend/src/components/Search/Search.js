275fc65bd12c162fe9a4ef97960ab5895ab15c79
Hannah Amundson <hannahkamundson@gmail.com>
2019-07-02 23:33:49 -0600

    Modified search to fit what we are needing

--- frontend/src/components/Search/Search.js
+++ frontend/src/components/Search/Search.js
@@ -1,68 +1,25 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Field, reduxForm } from 'redux-form';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
+import React, { Component, useState } from "react";
+import { connect } from "react-redux";
+import {
+  MenuItem,
+  Select,
+  Button,
+  TextField
+} from "@material-ui/core";
 
-import './Search.css'
+function SearchBar(props) {
 
-class Search extends Component {
-    constructor(props) {
-        super(props)
-        this.handleChange = this.handleChange.bind(this);
-        this.state = {
-            selectedOption: 'All',
-        }
-    }
-
-    handleChange(event) {
-        this.setState({
-            selectedOption: event.target.value
-        })
-    }
-
-    render() {
-        const options = ['All', 'Student', 'Course', 'School', 'District', 'Program']
-      
-        return (
-            <div className="form-wrapper">
-                <form className="search-form">
-                    <Field 
-                        name="search-input" 
-                        label="Search box"
-                        variant="outlined"
-                        required="True"
-                        className="search-input"
-                        component={TextField} />               
-                    <Select 
-                        name="search-options"
-                        required="True"
-                        label="Search Type"
-                        variant="outlined"
-                        className="search-options"
-                        onChange={this.handleChange}
-                        value={this.state.selectedOption}>
-                        {options.map((item, index) => (
-                            <MenuItem value={item} key={index}>{item}</MenuItem>
-                        ))}
-                    </Select>
-                    <Button 
-                        size="small"
-                        variant="contained"
-                        color="secondary"
-                        className="submit-button">
-                        Submit
-                    </Button>                
-                </form>
-            </div>
-        )
-    }
+  return (
+    <>
+      <TextField
+        name="searchInput"
+        label="Search box"
+        variant="outlined"
+        required="true"
+        onChange={props.action}
+      />
+    </>
+  );
 }
 
-const ReduxFormSearch = reduxForm({
-    form: 'search',
-})(Search)
-
-export default connect()(ReduxFormSearch)
+export default SearchBar;
