7c30672141e4dd7b59af6db0275ef0e750f41aeb
Hannah Amundson <hannahkamundson@gmail.com>
2019-07-06 13:49:54 -0600

    Added page for modify my students

--- frontend/src/pages/ManageData/MyStudents.js
+++ frontend/src/pages/ManageData/MyStudents.js
@@ -1,38 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchStudents } from 'state/StudentActions';
-import PropTypes from 'prop-types';
-import { Typography, withStyles } from '@material-ui/core';
-import { loadingJSX } from 'components/sharedStyles/LoadingStyles';
-import { TablePageStyles } from 'components/sharedStyles/Table/TablePageStyles';
-import { CreateStudentTable } from 'components/sharedStyles/Table/CreateTablesStyle';
-import MyStudentsMultiCheckbox from '../../components/ManageData/MyStudentsMultiCheckbox';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchStudents } from "state/StudentActions";
+import PropTypes from "prop-types";
+import { Typography, withStyles, Button } from "@material-ui/core";
+
+import { TablePageStyles } from "components/sharedStyles/Table/TablePageStyles";
+import {
+  MyStudentsToAdd,
+  MyStudentsToRemove
+} from "../../components/sharedStyles/ManageData/MyStudentsMultiCheckbox";
 
 function MyStudents(props) {
   const {
     classes: { header, striped, tHead, tRow }
   } = props;
 
-  const [loading, setLoading] = useState(true);
-  const students = useSelector(state => state.students.students);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchStudents({ accessLevel: 'my' }));
-    setLoading(false);
-  }, [dispatch]);
-
-  if (loading) {
-    return loadingJSX('Students');
-  }
-
   return (
     <div>
       <Typography variant="h4" component="h1" className={header}>
-        Add/Remove My Students
+        Add/Remove Students from My Students List
       </Typography>
-      <MyStudentsMultiCheckbox />
-      
+
+      <Typography variant="h6" component="h1" className={header}>
+        Add Students To My List
+      </Typography>
+      <MyStudentsToAdd />
+      <br />
+
+      <br />
+      <br />
+      <Typography variant="h6" component="h1" className={header}>
+        Remove Students From My List
+      </Typography>
+      <MyStudentsToRemove />
     </div>
   );
 }
